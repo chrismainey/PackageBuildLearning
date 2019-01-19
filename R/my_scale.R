4d62df048cb5384b23b740f0efea5d45a723ca51
Chris Mainey <chrismainey@gmail.com>
2019-01-19 20:26:21 +0000

    First package commit

---
+++ R/my_scale.R
@@ -0,0 +1,15 @@
+#' My Scale wrapper for Gelman-style scaling
+#'
+#' @description A wrapper for the rescale function from the \code{arm} package.  This mean centres the data and devides by 2*standard deviation.
+#'
+#' @param x vector
+#'
+#' @return vector of scaled inputs
+#' @export
+#'
+#' @importFrom arm rescale
+#'
+#' @examples my_scale(x)
+my_scale <- function(x){
+  arm::rescale(x)
+}
